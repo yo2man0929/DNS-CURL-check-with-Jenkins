10d8da380d2ab4d352683144457d6d0617a42a84
joe_yang <yo2man0929@gmail.com>
2023-11-02 17:15:18 +0800

    Revise the logics

--- check-dns-curl.sh
+++ check-dns-curl.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-URL_FILE="urls.txt"
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+LOCAL_URL_FILE="./urls.txt" # 方便local測試
+JENKINS_URL_FILE="/var/jenkins_home/urls.txt"
+SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+
+CHECKING_DOMAIN=$1
+
+
+if [ -f "$LOCAL_URL_FILE" ]; then
+  URL_FILE="$LOCAL_URL_FILE"
+else
+  URL_FILE="$JENKINS_URL_FILE"
+fi
+
 
 check_command() {
   if ! command -v "$1" > /dev/null 2>&1; then
@@ -15,7 +26,7 @@ post_to_slack() {
 }
 
 check_http_service() {
-  http_status=$(curl -s -o /dev/null -w '%{http_code}' "$1")
+  http_status=$(curl -s -o /dev/null -w '%{http_code}' --max-time 2 "$1")
   if [ "$http_status" -ge 200 ] && [ "$http_status" -lt 400 ]; then
     echo "ok"
   else
@@ -24,29 +35,30 @@ check_http_service() {
 }
 
 check_dns_resolution() {
-  if dig "$1" +short > /dev/null; then
+  if dig +time=2 +retry=0 "$1" +short A | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' > /dev/null; then
     echo "ok"
   else
     echo "fail"
   fi
 }
 
-# Check if required commands are available
+
 check_command curl
 check_command dig
 
-# Ensure the URL file exists
+
 if [ ! -f "$URL_FILE" ]; then
   echo "Error: File does not exist: $URL_FILE" >&2
   exit 1
 fi
 
-# Prepare the results variable and timestamp
+
 results=""
 TIMESTAMP=$(date +%Y%m%d%H%M)
+mkdir -p /var/jenkins_home/log/ || true
 OUTPUT_FILE="/tmp/check_results_${TIMESTAMP}"
 
-# Read URLs from the file and check them
+
 while IFS= read -r url; do
   http_status=$(check_http_service "$url")
   dns_status=$(check_dns_resolution "$url")
@@ -55,8 +67,11 @@ while IFS= read -r url; do
   echo "${line}" >> "${OUTPUT_FILE}.log"
 done < "$URL_FILE"
 
-# Read additional domains provided as command-line arguments separated by commas
-echo "$1" | tr ',' '\n' | while read domain; do
+# 檢查額外定義的domain
+OLD_IFS="$IFS"
+IFS=',' # 设置 IFS 分隔號為逗號
+set -f # 禁用路徑名擴展
+for domain in $CHECKING_DOMAIN; do
   if [ -n "$domain" ]; then
     http_status=$(check_http_service "$domain")
     dns_status=$(check_dns_resolution "$domain")
@@ -65,8 +80,10 @@ echo "$1" | tr ',' '\n' | while read domain; do
     echo "${line}" >> "${OUTPUT_FILE}.log"
   fi
 done
+set +f # 重新啟用路徑名擴展
+IFS="$OLD_IFS" # 還原 IFS
 
-# Format the results as a JSON object and post to Slack
+
 json_payload=$(printf '{"text":"Results:\n%s"}' "$results")
 post_to_slack "$json_payload"
 
