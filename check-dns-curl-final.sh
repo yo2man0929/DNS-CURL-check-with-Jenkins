62cc31a29ba598b2a78305d065d1dae315221b4a
joe_yang <yo2man0929@gmail.com>
2023-11-04 00:10:50 +0800

    Install pkg using script

--- check-dns-curl-final.sh
+++ check-dns-curl-final.sh
@@ -4,7 +4,7 @@ set -x
 
 LOCAL_URL_FILE="./urls.txt"
 JENKINS_URL_FILE="/var/jenkins_home/urls.txt" # 記得把urls.txt放到/var/jenkins_home/底下
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+SLACK_WEBHOOK_URL=`cat slack.key`
 
 # 方便測試，可以直接在這裡設定要測試的domain
 if [ -z "$1" ] && [ ! -z "$CHECKING_DOMAIN" ]; then
@@ -18,13 +18,15 @@ if [ -f "$LOCAL_URL_FILE" ]; then
   URL_FILE="$LOCAL_URL_FILE"
 else
   URL_FILE="$JENKINS_URL_FILE"
+  if [ -f "$JENKINS_URL_FILE" ]; then
+  echo "1919.com" > /var/jenkins_home/urls.txt
+  fi
 fi
 
 
 check_command() {
   if ! command -v "$1" > /dev/null 2>&1; then
-    echo "Error: $1 is required but not installed." >&2
-    exit 1
+    apt-get install "$1" -y
   fi
 }
 
@@ -41,9 +43,9 @@ check_http_service() {
 
   status_code=$(curl -s -o /dev/null -w '%{http_code}' --max-time 5 "$url")
   effective_url=$(curl -Ls -o /dev/null -w '%{url_effective}' --max-time 5 "$url")
-  # 兩邊格式不一樣，所以要轉換
   url_without_scheme=$(echo $effective_url | sed -E 's,https?://,,; s,/.*,,g')
 
+  # If the effective URL is different from the original URL, a redirect has occurred
   if [ "$url" != "$url_without_scheme" ]; then
     echo "${status_code}|REDIRECT|${url_without_scheme}"
   else
@@ -80,11 +82,6 @@ check_command dig
 check_command awk
 check_command sed
 
-# 確認urls.txt存在
-if [ ! -f "$URL_FILE" ]; then
-  echo "Error: File does not exist: $URL_FILE" >&2
-  exit 1
-fi
 
 # 初始化變數
 results=""
@@ -103,9 +100,7 @@ while IFS= read -r url; do
   dns_status=$(check_dns_resolution "$url")
   line="${url}_curl: ${http_status}, ${url}_dns: ${dns_status}"
 
-  # If there was a redirect, then the URL has changed
   if [ "$redirect_info" = "REDIRECT" ]; then
-    # Since it's a redirect, we need to extract the redirect URL using the Location header
     final_url=$(echo "$result" | cut -d'|' -f3)
     if [ -n "$final_url" ]; then
       redirect_http_status=$(curl -s -o /dev/null -w '%{http_code}' --max-time 2 "$final_url")
@@ -155,19 +150,20 @@ format_results() {
     | sed -E 's/_curl: 3[0-9]{2}/_curl: ok/g' \
     | sed -E 's/_curl: 2[0-9]{2}/_curl: ok/g' \
     | sed -E 's/_curl: 4[0-9]{2}/_curl: ok/g' \
-    | sed -E 's/_curl: 5[0-9]{2}/_curl: not ok/g' \
+    | sed -E 's/_curl: 5[0-9]{2}/_curl: fail/g' \
     | sed -E 's/_curl: 000/_curl: fail/g' \
     | sed -E 's/redirect_curl: 2[0-9]{2}/redirect_curl: ok/g' \
     | sed -E 's/redirect_curl: 4[0-9]{2}/redirect_curl: ok/g' \
-    | sed -E 's/redirect_curl: 5[0-9]{2}/redirect_curl: not ok/g' \
+    | sed -E 's/redirect_curl: 5[0-9]{2}/redirect_curl: fail/g' \
     | sed -E 's/redirect_curl: 000/redirect_curl: fail/g'
 }
 formatted_results=$(format_results "$results")
+
 # Post results to Slack and the alert server
-#json_payload=$(printf '{"text":"Results:\n%s"}' "$formatted_results")
+json_payload=$(printf '{"text":"Results:\n%s"}' "$formatted_results")
+
 # Uncomment the following line to enable Slack posting
-#post_to_slack "$json_payload"
+post_to_slack "$json_payload"
 
-post_to_alert_server "Domain Checking! curl_404 is ok!" "$formatted_results"
+#post_to_alert_server "Domain Checking! curl_404 is ok!" "$formatted_results"
 
-# End of script
